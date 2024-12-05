1b17b6c346fc9704069a9a068930b854f5072f21
DorianZGIS <51403878+DorianZGIS@users.noreply.github.com>
2024-12-05 13:54:55 +0000

    adjusted setup file

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ except FileNotFoundError:
 
 # Metadata
 setup(
-  name = "Type here the name of your package",
+  name = "dorianpy",
   version = 0.0.0.9000,
-  author = "Type here your name",
+  author = "dorian",
   author_email = "Type here your email",
-  description = "Describe here your package in one sentence",
+  description = "An incredibly amazing package",
   license = "Type here what license your package has",
   packages = find_packages(),
   install_requires = requirements
