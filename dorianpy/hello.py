f93cda103fb6c80c4a11b74b0854035420279a36
DorianZGIS <51403878+DorianZGIS@users.noreply.github.com>
2024-12-05 14:07:21 +0000

    do not forget

--- dorianpy/hello.py
+++ dorianpy/hello.py
@@ -1,2 +1,20 @@
 def hello(username = "anon"):
+  """Write here a title of your function
+  
+  Write here a short description of your function.
+  
+  Parameters
+  ----------
+  username : str
+    Describe here what the name parameter does.
+
+  Returns
+  -------
+  str
+    Describe here what the function returns.
+  
+  Examples
+  --------
+  >>> Give here an example of how to use the function.
+  """
   print("Hello ", username, "! Welcome to the best python Python package in the entire universe!", sep = "")
